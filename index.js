40917524187f78fb434f6c4dc16f51f9b0c3f42e
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-08 23:53:40 -0500

    got a little further

--- index.js
+++ index.js
@@ -7,10 +7,33 @@ class Student {
   }
 }
 
+//thi worked not so badly but it's not completely correct and I don't know how to test it
 function mostMoney(students) {
   // your code here
-  let args = { ...students };
-  console.log(args);
+  //it looks like they are arrays nested with objects
+  //so i think i can loop through this
+  //everyone's money
+  let totalMoney = [];
+  let studentWithMost = null;
+  for (let i = 0; i < students.length; i++) {
+    totalMoney.push(
+      students[i].fives * 5 + students[i].tens * 10 + students[i].twenties * 20
+    );
+  }
+
+  let moneyInOrder = totalMoney.concat().sort();
+  //the problem is that sort mutates totalMoney and we do not want that
+  //the largest number should be the last of the moneyOrder array but only if we sort it properly
+  //go through totalMoney array to
+  for (let i = 0; i < totalMoney.length; i++) {
+    if (totalMoney[i] === moneyInOrder[moneyInOrder.length - 1]) {
+      studentWithMost = students[i].name;
+    }
+  }
+  console.log(totalMoney);
+  console.log(moneyInOrder);
+
+  return studentWithMost;
 }
 
 const andy = new Student("Andy", 0, 0, 2);
